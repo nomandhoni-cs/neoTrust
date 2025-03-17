ab22b90655288988e99b7fa673d407d7c4570b6f
Noman Dhoni <nomandhoni@yahoo.com>
2025-03-17 14:41:53 +0600

    Fixing node_modules

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './*.html',
+    './js/**/*.js',
+    './public/**/*.css',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
